f8f330473989ec0d40da368af55dc87eb9923d64
alvarogonzalezferrer <alvarogonzalezferrer@users.noreply.github.com>
2022-11-11 16:39:41 -0600

    new menu system
    alfa version, not tested yet

---
+++ INCLUDE/menu.h
@@ -0,0 +1,80 @@
+// -----------------------------------------------------------------------
+// menu.h
+// -----------------------------------------------------------------------
+// Menu system for the game
+// DEBUG: Very basic so far 
+// -----------------------------------------------------------------------
+// Copyright (c) 2022, Kronoman
+// In loving memory of my father.
+// -----------------------------------------------------------------------
+// Released under The MIT License.
+// -----------------------------------------------------------------------
+#ifndef MENU_GAME_H
+#define MENU_GAME_H
+
+#include <allegro.h>
+
+//#include <iostream>
+#include <string>
+#include <vector>
+
+class Menu 
+{
+	public: 
+		Menu();
+		~Menu();
+		
+		// menu items should already be loaded in the vector when calling this!
+		// also any init like colors, gap, etc
+		void init(); // call once before starting the menu rendering
+		
+		// call each frame
+		// will return menu item selected (0..n-1) or -1 if everything all right, -2 if cancelled by used (ESC, etc)
+		int update(); 
+		
+		void render(BITMAP *bmp); // render menu on bitmap
+		
+	    // stuff
+		
+		// internal
+		int frame; // frame counter 
+		int wait; // wait counter
+		int selected; // current selected item 		
+		int w, h; // dimensions in pixels of current menu (calculated on init)
+		
+		
+		// ** SETUP THE FOLLOWING BEFORE STARTING MENU!! **
+		// font
+		FONT *fm; // menu font, defaults to Allegro font
+		
+		// colors 
+		// non selected text
+		int fg; // text color 
+		int bg; // text background
+		
+		// selected text
+		int fg_s; // text color
+		int bg_s; // text background 
+		
+		// position on bitmap to render
+		int x,y; // middle top position of menu, if -1 will be auto calculated
+		int gap; // y gap between items
+		
+		
+		// items of the menu
+		// DEBUG FOR NOW THEY SHOULD FIT ALL INSIDE SCREEN OR ELSE WILL NOT BE SHOWN		
+		std::vector<std::string> menuItems;
+		
+		/*** DEV NOTE
+		remember how to use vector strings: 
+		
+			vector<string> animal_vec;
+			animal_vec.push_back("Elephant");
+			animal_vec.push_back("Lion");
+			animal_vec.push_back("Deer");
+			animal_vec.push_back("Tiger");
+			for(int i = 0; i<animal_vec.size(); i++) {
+			  cout << animal_vec[i] << endl;
+		*******/
+};
+#endif
---
+++ SRC/menu.cpp
@@ -0,0 +1,150 @@
+// -----------------------------------------------------------------------
+// menu.cpp
+// -----------------------------------------------------------------------
+// Menu system for the game
+// -----------------------------------------------------------------------
+// Copyright (c) 2022, Kronoman
+// In loving memory of my father.
+// -----------------------------------------------------------------------
+// Released under The MIT License.
+// -----------------------------------------------------------------------
+
+#include "menu.h"
+
+Menu::Menu()
+{
+	// init
+	fm = NULL; // default allegro font
+	
+	fg = bg = fg_s = bg_s = -1; // colors default
+	x = y = gap = -1;
+	
+	w = -1;
+	h = 0;
+}
+
+Menu::~Menu()
+{
+	// end
+}
+
+// INIT
+// menu items should already be loaded in the vector when calling this!
+// also any init like colors, gap, etc
+void Menu::init()
+{
+	// initialize position of items, etc
+	selected = wait = frame = 0;
+	
+	if (!fm)
+	{
+		fm = font; // default allegro font
+		gap = -1; // set default gap
+	}
+	
+	if (gap < 0)
+	{
+		gap = text_height(fm)+3;
+	}
+	
+	
+	
+	if (fg < 0)
+	{
+		// set default colors 
+		fg = makecol(255,255,255);
+		bg = -1;
+		
+		fg_s = makecol(255,255,85);
+		bg_s = makecol(170,0,0);
+	}
+	
+	// calculate dimensiones of the menu
+	w = 0; // calculate
+	h = 0;
+	int rh = text_height(fm);
+	for(int i = 0; i<menuItems.size(); i++)
+	{
+		int rw = text_length(fm, menuItems[i].c_str());
+		if (rw > w)
+			w = rw;
+		
+		h += rh + gap;
+	}
+	
+	// DEBUG CALCULATE ACCORDING TO MAX MENU HEIGHT
+	if (x < 0 || y < 0)
+	{
+		x = 160;
+		y = 100; 
+	}
+}
+
+int Menu::update()
+{
+	// update logic
+	if (wait < 1)
+	{
+		if (key[KEY_W] || key[KEY_UP] || key[KEY_A] || key[KEY_LEFT])
+		{
+			selected--;
+			wait = 3; // slow down 
+		}
+
+		if (key[KEY_S] || key[KEY_DOWN] || key[KEY_D] || key[KEY_RIGHT])
+		{
+			selected++;
+			wait = 3; // slow down 
+		}
+	}
+	else
+	{
+		wait--;
+	}
+
+	if (selected < 0)
+		selected = menuItems.size()-1;
+	
+	if (selected > menuItems.size()-1)
+		selected = 0;
+
+	if (key[KEY_SPACE] || key[KEY_Z] || key[KEY_ENTER] || key[KEY_X])
+		return selected;
+	
+	if (key[KEY_ESC])
+		return -2 ; // cancel
+	
+	return -1; // all OK
+}
+
+
+void Menu::render(BITMAP *bmp)
+{
+	// render on bitmap
+	int c_f = fg;
+	int c_b = bg;
+	
+	// real y to start drawing
+	int ry = y - h/2;
+	
+	// step y
+	int rys = text_height(fm) + gap;
+	
+	for(int i = 0; i<menuItems.size(); i++)
+	{
+		if (i == selected)
+		{
+			c_f = fg_s;
+			b_f = bg_s;
+		}
+		else
+		{
+			c_f = fg;
+			c_b = bg;
+		}
+		
+		 textout_centre_ex(bmp, fm, menuItems[i].c_str(), x, ry, c_f, c_b);
+		 
+		 ry += rys;
+	}
+}
